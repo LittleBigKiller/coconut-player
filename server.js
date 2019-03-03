a3f7a41623995b8e5d6786f886116b12cd13fb6c
LittleBigKiller <35974496+LittleBigKiller@users.noreply.github.com>
2019-03-03 15:03:41 +0100

    file structure cleanup

--- server.js
+++ server.js
@@ -1,83 +1,86 @@
 var http = require("http")
 var qs = require("querystring")
 var fs = require("fs")
+var PORT = 5500
+
+var albumPaths = []
+var trackPaths = []
 
 var server = http.createServer(function(req,res){
     console.log(req.method + ' ' + req.url)
-    //let filename = req.url.split('/')
-    //filename = filename[filename.length - 1]
-    //let decoded = decodeURIComponent(filename)
     switch (req.method) {
         case "GET":
-            if (req.url.indexOf(".mp3") != -1) {
-                fs.readFile(__dirname + decodeURI(req.url), function (error, data) {
-                    res.writeHead(200, { "Content-type": "audio/mpeg; charset=utf-8" })
-                    res.write(data)
-                    res.end()
-                })
-            } else if (req.url.indexOf(".wav") != -1) {
-                fs.readFile(__dirname + decodeURI(req.url), function (error, data) {
-                    res.writeHead(200, { "Content-type": "audio/wav; charset=utf-8" })
-                    res.write(data)
-                    res.end()
-                })
-            } else if (req.url.indexOf(".js") != -1) {
-                fs.readFile(__dirname + decodeURI(req.url), function (error, data) {
-                    res.writeHead(200, { "Content-type": "aplication/javascript; charset=utf-8" })
-                    res.write(data)
-                    res.end()
-                })
-            } else if (req.url.indexOf(".css") != -1) {
-                fs.readFile(__dirname + decodeURI(req.url), function (error, data) {
-                    res.writeHead(200, { "Content-type": "text/css; charset=utf-8" })
-                    res.write(data)
-                    res.end()
-                })
-            } else if (req.url.indexOf(".jpg") != -1) {
-                fs.readFile(__dirname + decodeURI(req.url), function (error, data) {
-                    if (error) {
-                        fs.readFile(__dirname + '/static/covers/default.jpg', function (error, dataDef) {
-                            res.writeHead(200, { 'Content-Type': 'image/jpeg; charset=utf-8' });
-                            res.write(dataDef);
-                            res.end();
-                        })
-                    } else {
-                        res.writeHead(200, { "Content-type": "image/jpeg; charset=utf-8" })
-                        res.write(data)
-                        res.end()
-                    }
-                })
-            } else if (req.url.indexOf(".png") != -1) {
-                fs.readFile(__dirname + decodeURI(req.url), function (error, data) {
-                    res.writeHead(200, { "Content-type": "image/png; charset=utf-8" })
-                    res.write(data)
-                    res.end()
-                })
-            } else {
-                fs.readFile(__dirname + "/static/index.html", function (error, data) {
-                    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
-                    res.write(data)
-                    res.end()
-                })
-            }
-            break;
+            getResponse(req, res)
+            break
         case "POST":
             res.writeHead(200,{"content-type":"text/html; charset=utf-8"})
-            servResponse(req, res)
-            break;
-
+            postResponse(req, res)
+            break
     } 
 })
 
-server.listen(5500, function(){
-   console.log("serwer startuje na porcie 5500")
+server.listen(PORT, function(){
+   console.log("serwer startuje na porcie " + PORT)
 })
 
-function servResponse(req, res) {
+function getResponse(req, res) {
+    if (req.url.indexOf(".mp3") != -1) {
+        fs.readFile(__dirname + decodeURI(req.url), function (error, data) {
+            res.writeHead(200, { "Content-type": "audio/mpeg; charset=utf-8" })
+            res.write(data)
+            res.end()
+        })
+    } else if (req.url.indexOf(".wav") != -1) {
+        fs.readFile(__dirname + decodeURI(req.url), function (error, data) {
+            res.writeHead(200, { "Content-type": "audio/wav; charset=utf-8" })
+            res.write(data)
+            res.end()
+        })
+    } else if (req.url.indexOf(".js") != -1) {
+        fs.readFile(__dirname + "/static/" + decodeURI(req.url), function (error, data) {
+            res.writeHead(200, { "Content-type": "aplication/javascript; charset=utf-8" })
+            res.write(data)
+            res.end()
+        })
+    } else if (req.url.indexOf(".css") != -1) {
+        fs.readFile(__dirname + "/static/" + decodeURI(req.url), function (error, data) {
+            res.writeHead(200, { "Content-type": "text/css; charset=utf-8" })
+            res.write(data)
+            res.end()
+        })
+    } else if (req.url.indexOf(".jpg") != -1) {
+        fs.readFile(__dirname + decodeURI(req.url), function (error, data) {
+            if (error) {
+                fs.readFile(__dirname + '/static/covers/default.jpg', function (error, dataDef) {
+                    res.writeHead(200, { 'Content-Type': 'image/jpeg; charset=utf-8' });
+                    res.write(dataDef);
+                    res.end();
+                })
+            } else {
+                res.writeHead(200, { "Content-type": "image/jpeg; charset=utf-8" })
+                res.write(data)
+                res.end()
+            }
+        })
+    } else if (req.url.indexOf(".png") != -1) {
+        fs.readFile(__dirname + decodeURI(req.url), function (error, data) {
+            res.writeHead(200, { "Content-type": "image/png; charset=utf-8" })
+            res.write(data)
+            res.end()
+        })
+    } else {
+        fs.readFile(__dirname + "/static/index.html", function (error, data) {
+            res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
+            res.write(data)
+            res.end()
+        })
+    }
+}
+
+function postResponse(req, res) {
     var allData = "";
 
     req.on("data", function (data) {
-        //console.log("data: " + data)
         allData += data;
     })
 
